483285690c46551216857f969140d0ce15f7479e
Antonio Lechuga <antoniolechuga@MacBook-Pro-de-JALR.local>
2020-03-18 11:20:03 -0600

    Implement linked binary tree

---
+++ BinaryTree/LinkedBinaryTree.java
@@ -0,0 +1,125 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * <h1>LinkedBinaryTree Class</h1>
+ * This class implements a linked binary tree without balancing.
+ * It does not have an insert policy so the class in not functional.
+ * 
+ * @author Antonio Lechuga
+ * @version 1.0
+ * @since 2020-03-18
+ * @param <T>
+ */
+public class LinkedBinaryTree<T> implements BinaryTreeADT<T> {
+
+    @SuppressWarnings("unused")
+    private BinaryNode<T> sentinel, root;
+    private int counter;
+
+    LinkedBinaryTree() {
+        sentinel = new BinaryNode<T>(null);
+        root = null;
+        counter = 0;
+    }
+
+    public boolean isEmpty() {
+        return (counter == 0);
+    }
+
+    public int size() {
+        return counter;
+    }
+
+    public Iterator<T> inOrder() {
+        ArrayList<T> list = new ArrayList<T>();
+        inOrder(root, list);
+        return list.iterator();
+    }
+
+    private void inOrder(BinaryNode<T> actual, ArrayList<T> list) {
+        if (actual == null)
+            return;
+        inOrder(actual.getLeft(), list);
+        list.add(actual.getElement());
+        inOrder(actual.getRight(), list);
+    }
+
+    public Iterator<T> postOrder() {
+        ArrayList<T> list = new ArrayList<T>();
+        postOrder(root, list);
+        return list.iterator();
+    }
+
+    private void postOrder(BinaryNode<T> actual, ArrayList<T> list) {
+        if (actual == null)
+            return;
+        postOrder(actual.getLeft(), list);
+        postOrder(actual.getRight(), list);
+        list.add(actual.getElement());
+    }
+
+    public Iterator<T> preOrder() {
+        ArrayList<T> list = new ArrayList<T>();
+        preOrder(root, list);
+        return list.iterator();
+    }
+
+    private void preOrder(BinaryNode<T> actual, ArrayList<T> list) {
+        if (actual == null)
+            return;
+        list.add(actual.getElement());
+        preOrder(actual.getLeft(), list);
+        preOrder(actual.getRight(), list);
+    }
+
+    public Iterator<T> byLevel() {
+        ArrayList<T> list = new ArrayList<T>();
+        if (isEmpty())
+            return list.iterator();
+        
+        LinkedList<BinaryNode<T>> queue = new LinkedList<BinaryNode<T>>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            BinaryNode<T> actual = queue.removeFirst();
+            list.add(actual.getElement());
+            if (actual.getLeft() != null)
+                queue.add(actual.getLeft());
+            if (actual.getRight() != null)
+                queue.add(actual.getRight());
+        }
+
+        return list.iterator();
+    }
+
+    public boolean contains(T element) {
+        Iterator<T> inOrderIterator = inOrder();
+        while (inOrderIterator.hasNext()) {
+            T temp = inOrderIterator.next();
+            if (temp == element)
+                return true;
+        }
+        return false;
+    }
+
+    public T find(T element) {
+        Iterator<T> inOrderIterator = inOrder();
+        while (inOrderIterator.hasNext()) {
+            T temp = inOrderIterator.next();
+            if (temp == element)
+                return temp;
+        }
+        return null;
+    }
+
+    // Uncomment to debug
+    // public void setRoot(BinaryNode<T> node) {
+    //     root = node;
+    //     sentinel.setRight(root);
+    //     root.setFather(sentinel);
+    //     counter = node.numberOfDescendents() + 1;
+    // }
+}
