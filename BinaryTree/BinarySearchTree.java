2ce27455bb75c06c54677e353f2d242e766cb506
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-03-19 20:58:27 -0600

    Add insert method and minor fixes

--- BinaryTree/BinarySearchTree.java
+++ BinaryTree/BinarySearchTree.java
@@ -8,7 +8,10 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
 
     @Override
     public boolean contains(T element) {
-        return contains(this.getRoot(), element);
+        if (this.getRoot() == null)
+            return false;
+        else
+            return contains(this.getRoot(), element);
     }
 
     private boolean contains(BinaryNode<T> actual, T element) {
@@ -21,22 +24,41 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
         if (element.compareTo(actual.getElement()) < 0 && actual.getLeft() != null)
             return contains(actual.getLeft(), element);
         else if (actual.getRight() != null)
-            return contains(actual.getFather(), element);
+            return contains(actual.getRight(), element);
         else
             return false;
     }
 
     @Override
     public T find(T element) {
-        if (contains(this.getRoot(), element))
+        if (this.contains(element))
             return element;
         else
             return null;
     }
 
     public void insert(T element) {
-        // TODO Auto-generated method stub
+        if (getRoot() != null)
+            insert(this.getRoot(), element);
+        else
+            setRoot(new BinaryNode<T>(element));
+    }
 
+    private void insert(BinaryNode<T> actual, T element) {
+        if (element == null)
+            return;
+
+        if (element.compareTo(actual.getElement()) < 0) {
+            if (actual.getLeft() == null)
+                actual.hang(element);
+            else
+                insert(actual.getLeft(), element);
+        } else {
+            if (actual.getRight() == null)
+                actual.hang(element);
+            else
+                insert(actual.getRight(), element);
+        }
     }
 
     public T delete(T element) {
