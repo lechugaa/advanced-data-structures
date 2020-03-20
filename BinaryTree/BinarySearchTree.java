68b7d9a732d0e3717afc7bacc03a6c8607a072b2
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-03-19 20:09:32 -0600

    Override contains and find methods

--- BinaryTree/BinarySearchTree.java
+++ BinaryTree/BinarySearchTree.java
@@ -2,6 +2,38 @@ package BinaryTree;
 
 public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<T> implements BinarySearchTreeADT<T> {
 
+    BinarySearchTree() {
+        super();
+    }
+
+    @Override
+    public boolean contains(T element) {
+        return contains(this.getRoot(), element);
+    }
+
+    private boolean contains(BinaryNode<T> actual, T element) {
+        if (element == null)
+            return false;
+
+        if (element == actual.getElement())
+            return true;
+
+        if (element.compareTo(actual.getElement()) < 0 && actual.getLeft() != null)
+            return contains(actual.getLeft(), element);
+        else if (actual.getRight() != null)
+            return contains(actual.getFather(), element);
+        else
+            return false;
+    }
+
+    @Override
+    public T find(T element) {
+        if (contains(this.getRoot(), element))
+            return element;
+        else
+            return null;
+    }
+
     public void insert(T element) {
         // TODO Auto-generated method stub
 
