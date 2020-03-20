926efffa79d9d173db6f440caf045a46bf4b8efc
Antonio Lechuga <antoniolechugarivera@gmail.com>
2020-03-20 10:29:36 -0600

    Implement binary search tree class

--- BinaryTree/BinarySearchTree.java
+++ BinaryTree/BinarySearchTree.java
@@ -19,15 +19,21 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
     /**
      * This method takes advantage of the structure of a search tree
      * to optimize search time.
+     * 
+     * @param element to search
+     * @return boolean whether element is contained in tree or not
      */
     @Override
     public boolean contains(T element) {
-        if (this.getRoot() == null)
+        if (this.isEmpty())
             return false;
         else
-            return contains(this.getRoot(), element);
+            return contains(getRoot(), element);
     }
 
+    // internal method used for contains public method
+    // it recursively searches for element comparing with actual.getElement()
+    // to see if the search should continue to the right or the left
     private boolean contains(BinaryNode<T> actual, T element) {
         if (element == null)
             return false;
@@ -44,8 +50,40 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
     }
 
     /**
+     * This method returns the node containing the given element.
+     * @param element the node has to have
+     * @return BinaryNode<T> containing the element or null
+     */
+    public BinaryNode<T> findNode(T element) {
+        if (isEmpty())
+            return null;
+        else
+            return findNode(element, getRoot());
+    }
+
+
+    // internal method used to implement public findNode
+    private BinaryNode<T> findNode(T element, BinaryNode<T> actual) {
+        if (element == null)
+            return null;
+
+        if (actual.getElement() == element)
+            return actual;
+
+        if (element.compareTo(actual.getElement()) < 0 && actual.getLeft() != null) 
+            return findNode(element, actual.getLeft());
+        else if (actual.getRight() != null)
+            return findNode(element, actual.getRight());
+        else
+            return null;
+    }
+
+    /**
      * This method takes advantage of the structure of a search tree
      * to optimize search time.
+     * 
+     * @return element or null if it is not contained in tree.
+     * @param element to search
      */
     @Override
     public T find(T element) {
@@ -56,11 +94,12 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
     }
 
     /**
-     * This mehtod inserts a new node into the binary search tree.
+     * This method inserts a new node into the binary search tree.
+     * It does not perform balancing.
      */
     public void insert(T element) {
-        if (getRoot() != null) {
-            insert(this.getRoot(), element);
+        if (!isEmpty()) {
+            insert(getRoot(), element);
             setCounter(size() + 1);
         } else
             setRoot(new BinaryNode<T>(element));
@@ -72,40 +111,89 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
 
         if (element.compareTo(actual.getElement()) < 0) {
             if (actual.getLeft() == null)
-                actual.hang(element);
+                actual.hang(new BinaryNode<T>(element));
             else
                 insert(actual.getLeft(), element);
         } else {
             if (actual.getRight() == null)
-                actual.hang(element);
+                actual.hang(new BinaryNode<T>(element));
             else
                 insert(actual.getRight(), element);
         }
     }
 
     public T delete(T element) {
-        // TODO Auto-generated method stub
-        return null;
+        BinaryNode<T> node = findNode(element);
+        if (node == null)
+            return null;
+
+        setCounter(size() - 1);
+
+        if (node.getLeft() == null && node.getRight() == null) {
+            // case when node is a leaf
+            BinaryNode<T> parent = node.getFather();
+            if (parent.getLeft() == node)
+                parent.setLeft(null);
+            else
+                parent.setRight(null);
+        } else {
+            if (node.getLeft() != null && node.getRight() != null) {
+                // node has both children
+                BinaryNode<T> actual = node.getRight();
+
+                while (actual.getLeft() != null)
+                    actual = actual.getLeft();
+
+                if (actual.getRight() == null)
+                    actual.getFather().setLeft(null);
+                else
+                    actual.getFather().hang(actual.getRight());
+
+                node.setElement(actual.getElement());
+            } else {
+                // case when node has one child
+                if (node.getLeft() != null && node.getRight() == null)
+                    node.getFather().hang(node.getLeft());
+                else
+                    node.getFather().hang(node.getRight());
+            }
+        }
+        return node.getElement();
     }
 
     public T deleteMin() {
-        // TODO Auto-generated method stub
-        return null;
+        T element = findMin();
+        return delete(element);
     }
 
     public T deleteMax() {
-        // TODO Auto-generated method stub
-        return null;
+        T element = findMax();
+        return delete(element);
     }
 
     public T findMin() {
-        // TODO Auto-generated method stub
-        return null;
+        if (isEmpty())
+            return null;
+        return getMinNode(getRoot()).getElement();
+    }
+
+    private BinaryNode<T> getMinNode(BinaryNode<T> actual) {
+        if (actual.getLeft() == null)
+            return actual;
+        else
+            return getMinNode(actual.getLeft());
     }
 
     public T findMax() {
-        // TODO Auto-generated method stub
-        return null;
+        if (isEmpty())
+            return null;
+        return getMaxNode(getRoot()).getElement();
+    }
+
+    private BinaryNode<T> getMaxNode(BinaryNode<T> actual) {
+        if (actual.getRight() == null) 
+            return actual;
+        return getMaxNode(actual.getRight());
     }
     
 }
