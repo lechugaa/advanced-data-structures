865aea00f8a7624cd27d95a763a4928ea6723efc
Antonio Lechuga <antoniolechuga@MacBook-Pro-de-JALR.local>
2020-03-19 19:15:06 -0600

    Generate template

---
+++ BinaryTree/BinarySearchTree.java
@@ -0,0 +1,35 @@
+package BinaryTree;
+
+public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<T> implements BinarySearchTreeADT<T> {
+
+    public void insert(T element) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public T delete(T element) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public T deleteMin() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public T deleteMax() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public T findMin() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public T findMax() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
